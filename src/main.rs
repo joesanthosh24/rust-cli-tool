1404db02c7a1073a3edefbaa74f00fa1b04f23c0
joesanthosh24 <joe.santhosh88@yahoo.com>
2025-05-08 18:54:38 -0400

    Reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let _query:  &String = &args[1];
+    let filename: &String = &args[2];
+
+    let contents = fs::read_to_string(filename).expect("Error reading the file");
+
+    println!("Contents of file:\n{}", contents);
 }
