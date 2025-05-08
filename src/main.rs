8e8ccd1a9a8debd4a7e35d0b7ed3f8e0941ccad6
joesanthosh24 <joe.santhosh88@yahoo.com>
2025-05-08 19:08:27 -0400

    Refactored code to use a arguments config struct

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,23 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let _query:  &String = &args[1];
-    let filename: &String = &args[2];
+    let config = ArgsConfig::new(&args);
 
-    let contents = fs::read_to_string(filename).expect("Error reading the file");
+    let contents = fs::read_to_string(config.filename).expect("Error reading the file");
 
     println!("Contents of file:\n{}", contents);
+}
+
+struct ArgsConfig {
+    query: String,
+    filename: String
+}
+
+impl ArgsConfig {
+    fn new(args: &[String]) -> ArgsConfig {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        ArgsConfig { query, filename }
+    }
 }
