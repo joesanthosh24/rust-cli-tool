1193aecdb371f35781dd3f28b8ac8872f5f49724
joesanthosh24 <joe.santhosh88@yahoo.com>
2025-05-09 08:27:47 -0400

    Extracted code to read file to different function

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = ArgsConfig::new(&args);
+    let config = ArgsConfig::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(config.filename).expect("Error reading the file");
+    if let Err(e) = run(config) {
+        println!("Error: {e}");
+        process::exit(1);
+    }
+}
+
+fn run(config: ArgsConfig) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
 
     println!("Contents of file:\n{}", contents);
+
+    Ok(())
 }
 
 struct ArgsConfig {
@@ -16,10 +30,13 @@ struct ArgsConfig {
 }
 
 impl ArgsConfig {
-    fn new(args: &[String]) -> ArgsConfig {
+    fn new(args: &[String]) -> Result<ArgsConfig, &str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
     
-        ArgsConfig { query, filename }
+        Ok(ArgsConfig { query, filename })
     }
 }
